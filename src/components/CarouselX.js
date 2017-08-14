6caf3f0e21fa94f7306d85870facc4fbf8b0fc64
Chase Sinclair <chase.sinclair@cerner.com>
2017-08-14 17:04:19 -0500

    Comment out AWS config for deployment

--- src/components/CarouselX.js
+++ src/components/CarouselX.js
@@ -1,63 +1,68 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
-import { loadAssets } from '../utils/asset_loader';
+import furniture from '../images/fulls/furniture.jpg';
+import furniture2 from '../images/fulls/furniture2.jpg';
+import wine from '../images/fulls/wine_glass.jpg';
+import table from '../images/fulls/table.jpg';
+import welcome from '../images/fulls/welcome-to-our-home.jpg';
+// import { loadAssets } from '../utils/asset_loader';
 
 class CarouselX extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: null
-    };
-  }
-
-  componentWillMount() {
-    let urls = [];
-    loadAssets(urls)
-    .then(() => {
-      this.setState({
-        data: urls
-      });
-    });
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     data: null
+  //   };
+  // }
+  // 
+  // componentWillMount() {
+  //   let urls = [];
+  //   loadAssets(urls)
+  //   .then(() => {
+  //     this.setState({
+  //       data: urls
+  //     });
+  //   });
+  // }
 
   render() {
-    if (!this.state.data) {
-      return <div />
-    }
+    // if (!this.state.data) {
+    //   return <div />
+    // }
     return (
       <div>
         <Carousel>
           <Carousel.Item>
-            <img alt="900x500" src={this.state.data[0]}/>
+            <img alt="900x500" src={furniture}/>
             <Carousel.Caption>
               <h3>Quality Experience</h3>
               <p>Located on the historic park square of Paola, Simply You offers an experience like no other. We are a full-service salon and our professional staff are dedicated to providing you with the service and personalized treatment you deserve. Whether you are looking for a new look, have a special event, or just want a day for yourself, Simply You is certain to make you feel right at home every time.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img alt="900x500" src={this.state.data[1]}/>
+            <img alt="900x500" src={furniture2}/>
             <Carousel.Caption>
               <h3>Services</h3>
               <p>Services include: Hair Styling, Custom Hair Coloring, Nails, Facials, Waxing, Skincare, Makeup.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img alt="900x500" src={this.state.data[2]}/>
+            <img alt="900x500" src={wine}/>
             <Carousel.Caption>
               <h3>Check it out</h3>
               <p>Need a gift or something new for the season? From scarfs and purses to jewelry and make-up, come and find that trendy or new accessory for you or someone special.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img alt="900x500" src={this.state.data[3]}/>
+            <img alt="900x500" src={table}/>
             <Carousel.Caption>
               <h3>Gift Cards</h3>
               <p>Simply You gift cards and special packages make excellent gifts!</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img alt="900x500" src={this.state.data[4]}/>
+            <img alt="900x500" src={welcome}/>
             <Carousel.Caption>
               <h3>Slide label</h3>
               <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
