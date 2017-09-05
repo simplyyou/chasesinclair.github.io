cdf7e6d39c44a84ed0ee53941ef504f01c5425a6
Chase Sinclair <chase.sinclair@cerner.com>
2017-09-05 17:27:31 -0500

    Try react-router fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render((
-  <BrowserRouter>
+  <BrowserRouter basename="/simply-you" >
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
