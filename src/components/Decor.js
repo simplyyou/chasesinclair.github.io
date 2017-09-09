42a1e47da78633126cc7097fc7283d0932384726
Chase Sinclair <chase.sinclair@cerner.com>
2017-09-09 14:09:03 -0500

    Add 4th interior picture

--- src/components/Decor.js
+++ src/components/Decor.js
@@ -3,6 +3,7 @@ import { Grid, Row, Col, Thumbnail } from 'react-bootstrap';
 import interior1 from '../images/home_interior1.JPG';
 import interior2 from '../images/home_interior2.JPG';
 import interior3 from '../images/home_interior3.JPG';
+import interior4 from '../images/interiors4.jpg';
 
 
 class Decor extends Component {
@@ -21,6 +22,20 @@ class Decor extends Component {
             </Col>
             <Col xs={6} md={4}>
               <Thumbnail src={interior3} alt="Interiors">
+              </Thumbnail>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} md={4}>
+              <Thumbnail src={interior4} alt="Interiors">
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={4}>
+              <Thumbnail src={interior4} alt="Interiors">
+              </Thumbnail>
+            </Col>
+            <Col xs={6} md={4}>
+              <Thumbnail src={interior4} alt="Interiors">
               </Thumbnail>
             </Col>
           </Row>
