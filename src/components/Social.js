82fce1f844bffeb9c72125239cd20f7bc928ceaf
Chase Sinclair <chase.sinclair@cerner.com>
2017-09-09 11:44:41 -0500

    Update social media links

--- src/components/Social.js
+++ src/components/Social.js
@@ -4,9 +4,8 @@ class Social extends Component {
   render() {
     return (
     <div>
-      <a href="http://facebook.com/" className="btn btn-social-icon btn-lg btn-facebook"><i className="fa fa-facebook"></i></a>
-      <a href="http://twitter.com/" className="btn btn-social-icon btn-lg btn-twitter"><i className="fa fa-twitter"></i></a>
-      <a href="http://instagram.com/" className="btn btn-social-icon btn-lg btn-instagram"><i className="fa fa-instagram"></i></a>
+      <a href="https://www.facebook.com/simplyyousalonboutique/" className="btn btn-social-icon btn-lg btn-facebook"><i className="fa fa-facebook"></i></a>
+      <a href="https://www.instagram.com/simplyyousalon/" className="btn btn-social-icon btn-lg btn-instagram"><i className="fa fa-instagram"></i></a>
       <a href="http://pinterest.com/" className="btn btn-social-icon btn-lg btn-pinterest"><i className="fa fa-pinterest"></i></a>
     </div>
   );
